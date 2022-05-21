ecab977933522f73063382a3b7305e265d8c1127
Shivam Savani <shivambsavani@gmail.com>
2022-05-22 01:49:56 +0930

    new trial_1 9

--- selection/svd_classifier.py
+++ selection/svd_classifier.py
@@ -76,14 +76,24 @@ def fit_mixture(scores, labels, p_threshold=0.5):
 
     return np.array(clean_labels, dtype=np.int64), probs
 
+from sklearn.decomposition import PCA
+
+def get_score_shiv(current_features):
+
+    pca = PCA(n_components=2)
+
+    return pca.fit_transform(current_features)
+
 def fine(current_features, current_labels, fit='kmeans', previous_features=None, previous_labels=None, p_threshold=0.7):
 
     # if not previous_features and not previous_labels:
     #     singular_vector_dict = get_singular_vector(previous_features, previous_labels)
     # else:
-    singular_vector_dict = get_singular_vector(current_features, current_labels)
+    # singular_vector_dict = get_singular_vector(current_features, current_labels)
 
-    scores = get_score(singular_vector_dict, features=current_features, labels=current_labels)
+    # scores = get_score(singular_vector_dict, features=current_features, labels=current_labels)
+
+    scores = get_score_shiv(current_features, current_labels)
 
     if 'kmeans' in fit:
         clean_labels = cleansing(scores, current_labels)
