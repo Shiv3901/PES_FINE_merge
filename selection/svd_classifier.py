76ccf27101a916db60ffe4a1c1a9c6beceed3fe4
Shivam Savani <shivambsavani@gmail.com>
2022-05-28 23:03:48 +0930

    new version trial 18

--- selection/svd_classifier.py
+++ selection/svd_classifier.py
@@ -164,7 +164,7 @@ def cleansing(scores, labels):
             if i == 0:
                 counter += 1
 
-        print(counter, labels_.shape - counter)
+        print(counter, labels_.shape[0] - counter)
 
         # print(print_current_time("end: "))
 
