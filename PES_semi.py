7e1ee6d7c8e1e0439935bb49cf0b8bb29782c450
Shivam Savani <shivambsavani@gmail.com>
2022-06-05 22:14:08 +0930

    first attempt 14

--- PES_semi.py
+++ PES_semi.py
@@ -391,7 +391,7 @@ for epoch in range(args.num_epochs):
         print("Labels shape: ", labels.shape)
         confident_indexs, unconfident_indexs = helperFunctionForFINE(features, labels)
         print("Confident indexes shape: ", confident_indexs.shape)
-        labeled_trainloader, unlabeled_trainloader, class_weights = update_train_loader_shiv(features, labels, confident_indexs, unconfident_indexs)
+        labeled_trainloader, unlabeled_trainloader, class_weights = update_train_loader_shiv(data, noisy_labels, confident_indexs, unconfident_indexs)
 
         # labeled_trainloader, unlabeled_trainloader, class_weights = update_trainloader(model, data, clean_labels, noisy_labels, isFine)
 
