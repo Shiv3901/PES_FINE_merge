8f3b8ab0205478a15821ad7b2dfcbd557a6f8616
Shivam Savani <shivambsavani@gmail.com>
2022-06-05 21:14:21 +0930

    first attempt 14

--- selection/svd_classifier.py
+++ selection/svd_classifier.py
@@ -55,7 +55,7 @@ def get_features(model, dataloader):
     model.eval()
     model.cuda()
 
-    iter = iter(dataloader)
+    print(dataloader[0])
 
     with tqdm(dataloader) as progress:
         # pprint(vars(dataloader))
