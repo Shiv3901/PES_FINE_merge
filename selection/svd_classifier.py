6f9f845177e8bfac7ff704f1e6aaf620b04fac81
Shivam Savani <shivambsavani@gmail.com>
2022-06-06 22:51:10 +0930

    modify the files

--- selection/svd_classifier.py
+++ selection/svd_classifier.py
@@ -49,7 +49,7 @@ def get_features(model, dataloader):
 
     for i, data in enumerate(dataloader, 0):
         # get the inputs; data is a list of [inputs, labels]
-        print(data.shape)
+        print(data.size())
         return
         input, label = data
         input, label = input.cuda(), label.long()
