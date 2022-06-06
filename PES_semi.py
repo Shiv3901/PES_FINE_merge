6e3560f3884290b75fe2c86f79e6d9b15ef42ae1
Shivam Savani <shivambsavani@gmail.com>
2022-06-06 20:52:36 +0930

    modify the files

--- PES_semi.py
+++ PES_semi.py
@@ -397,13 +397,13 @@ for epoch in range(args.num_epochs):
             # print("Confident indexes shape: ", confident_indexs.shape)
             labeled_trainloader, unlabeled_trainloader, class_weights = update_train_loader_shiv(data, noisy_labels, confident_indexs, unconfident_indexs)
 
-            print("Length of the labeled trainloader: ", labeled_trainloader.length)
-            print("Length of the unlabeled trainloader: ", unlabeled_trainloader.length)
+            print("Length of the labeled trainloader: ", len(labeled_trainloader.dataset))
+            print("Length of the unlabeled trainloader: ", len(unlabeled_trainloader.dataset))
         else:
 
             labeled_trainloader, unlabeled_trainloader, class_weights = update_trainloader(model, data, clean_labels, noisy_labels, isFine)
-            print("Length of the labeled trainloader: ", labeled_trainloader.length)
-            print("Length of the unlabeled trainloader: ", unlabeled_trainloader.length)
+            print("Length of the labeled trainloader: ", len(labeled_trainloader.dataset))
+            print("Length of the unlabeled trainloader: ", len(unlabeled_trainloader.dataset))
 
         # mixmatch to learn from the clean models and make the noisy models correct 
         MixMatch_train(epoch, model, optimizer, labeled_trainloader, unlabeled_trainloader, class_weights)
