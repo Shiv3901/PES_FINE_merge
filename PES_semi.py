f5036d61b3a5acdff7a7c6fb5110781f74482dfd
Shivam Savani <shivambsavani@gmail.com>
2022-06-07 15:17:51 +0930

    modify the files

--- PES_semi.py
+++ PES_semi.py
@@ -435,9 +435,6 @@ for epoch in range(args.num_epochs):
             if noisy_labels[idx] == clean_labels[idx]:
                 true_labels += 1
 
-
-        print("True label count: " + str(true_labels))
-
         if isFine:
             # arguments required for mix match that update trainloader returns
             features, labels = get_features(model, train_loader)
@@ -452,11 +449,13 @@ for epoch in range(args.num_epochs):
 
             evaluate_accuracy(confident_indexs, unconfident_indexs, preds, clean_labels)
             # print("Confident indexes shape: ", confident_indexs.shape)
-            labeled_trainloader, unlabeled_trainloader, class_weights = update_train_loader_shiv(data, noisy_labels, confident_indexs, unconfident_indexs)
+            labeled_trainloader, unlabeled_trainloader, class_weights = update_train_loader_shiv(data, preds, confident_indexs, unconfident_indexs)
 
         else:
 
             labeled_trainloader, unlabeled_trainloader, class_weights = update_trainloader(model, data, clean_labels, noisy_labels, isFine)
+
+        print("True label count: " + str(true_labels))
 
         # print("Length of the labeled trainloader: ", len(labeled_trainloader.dataset))
         # print("Length of the unlabeled trainloader: ", len(unlabeled_trainloader.dataset))
