e50b7ad581cc9ceba02569e7283641c086f3fbca
Shivam Savani <shivambsavani@gmail.com>
2022-06-08 00:17:52 +0930

    modify the

--- selection/svd_classifier.py
+++ selection/svd_classifier.py
@@ -113,7 +113,7 @@ def fit_mixture(scores, labels, p_threshold=0.50):
         cls_index = indexes[labels==cls]
         feats = scores[labels==cls]
         feats_ = np.ravel(feats).astype(np.float).reshape(-1, 1)
-        gmm = GMM(n_components=2, covariance_type='full', tol=1e-6, max_iter=100)
+        gmm = GMM(n_components=2, covariance_type='full', tol=1e-6, max_iter=100, random_state=0)
         
         gmm.fit(feats_)
         prob = gmm.predict_proba(feats_)
